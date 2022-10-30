70e7ffdba28de9e306a0097924a6e1f6851c1477
ehmicky <ehmicky@gmail.com>
2022-10-30 17:50:08 +0100

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
-export default function modernErrorsWinston(value) {
-  return value
+import { getOptions } from './options.js'
+import { shortFormat, fullFormat } from './static.js'
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  name: 'winston',
+  getOptions,
+  staticMethods: { shortFormat, fullFormat },
 }
--- test/main.js
+++
@@ -1,6 +0,0 @@
-import test from 'ava'
-import modernErrorsWinston from 'modern-errors-winston'
-
-test('Dummy test', (t) => {
-  t.true(modernErrorsWinston(true))
-})
