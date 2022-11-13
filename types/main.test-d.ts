9a260d68d01591b8e632638275482f6b385180b3
ehmicky <ehmicky@gmail.com>
2022-11-13 15:52:10 +0100

    Improve types

--- types/main.test-d.ts
+++ types/main.test-d.ts
@@ -1,4 +1,4 @@
-import modernErrors from 'modern-errors'
+import ModernError from 'modern-errors'
 import modernErrorsWinston, { Options, Format } from 'modern-errors-winston'
 import {
   expectType,
@@ -8,40 +8,71 @@ import {
 } from 'tsd'
 import { createLogger } from 'winston'
 
-const BaseError = modernErrors([modernErrorsWinston])
+const BaseError = ModernError.subclass('BaseError', {
+  plugins: [modernErrorsWinston],
+})
 const fullFormat = BaseError.fullFormat()
 const shortFormat = BaseError.shortFormat()
 
-modernErrors([modernErrorsWinston], { winston: {} })
+ModernError.subclass('TestError', {
+  plugins: [modernErrorsWinston],
+  winston: {},
+})
 BaseError.fullFormat({})
 BaseError.shortFormat({})
 expectAssignable<Options>({})
 expectError(BaseError.fullFormat(undefined))
 expectError(BaseError.shortFormat(undefined))
 expectNotAssignable<Options>(undefined)
-expectError(modernErrors([modernErrorsWinston], { winston: true }))
+expectError(
+  ModernError.subclass('TestError', {
+    plugins: [modernErrorsWinston],
+    winston: true,
+  }),
+)
 expectError(BaseError.fullFormat(true))
 expectError(BaseError.shortFormat(true))
 expectNotAssignable<Options>(true)
-expectError(modernErrors([modernErrorsWinston], { winston: { unknown: true } }))
+expectError(
+  ModernError.subclass('TestError', {
+    plugins: [modernErrorsWinston],
+    winston: { unknown: true },
+  }),
+)
 expectError(BaseError.fullFormat({ unknown: true }))
 expectError(BaseError.shortFormat({ unknown: true }))
 expectNotAssignable<Options>({ unknown: true })
 
-modernErrors([modernErrorsWinston], { winston: { level: 'error' } })
+ModernError.subclass('TestError', {
+  plugins: [modernErrorsWinston],
+  winston: { level: 'error' },
+})
 BaseError.fullFormat({ level: 'error' })
 BaseError.shortFormat({ level: 'error' })
 expectAssignable<Options>({ level: 'error' })
-expectError(modernErrors([modernErrorsWinston], { winston: { level: true } }))
+expectError(
+  ModernError.subclass('TestError', {
+    plugins: [modernErrorsWinston],
+    winston: { level: true },
+  }),
+)
 expectError(BaseError.fullFormat({ level: true }))
 expectError(BaseError.shortFormat({ level: true }))
 expectNotAssignable<Options>({ level: true })
 
-modernErrors([modernErrorsWinston], { winston: { stack: true } })
+ModernError.subclass('TestError', {
+  plugins: [modernErrorsWinston],
+  winston: { stack: true },
+})
 BaseError.fullFormat({ stack: true })
 BaseError.shortFormat({ stack: true })
 expectAssignable<Options>({ stack: true })
-expectError(modernErrors([modernErrorsWinston], { winston: { stack: 'true' } }))
+expectError(
+  ModernError.subclass('TestError', {
+    plugins: [modernErrorsWinston],
+    winston: { stack: 'true' },
+  }),
+)
 expectError(BaseError.fullFormat({ stack: 'true' }))
 expectError(BaseError.shortFormat({ stack: 'true' }))
 expectNotAssignable<Options>({ stack: 'true' })
